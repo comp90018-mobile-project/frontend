e99e3919c37087a03cff82ece8bf22956fb17f0a
zhuoxuan jiang <jiangzhuoxuan97@gmail.com>
2022-09-29 18:53:26 +1000

    set up maps

---
+++ src/pages/Map/mapStyles.js
@@ -0,0 +1,16 @@
+import {
+  Dimensions, StyleSheet
+} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
