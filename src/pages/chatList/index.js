921bb9122a150df43b84720690b51114d91d16bb
zhuoxuan jiang <jiangzhuoxuan97@gmail.com>
2022-10-25 14:55:12 +1100

    fix navigation

--- src/pages/chatList/index.js
+++ src/pages/chatList/index.js
@@ -1,44 +1,44 @@
 import { getAuth } from 'firebase/auth';
 import React from 'react';
-import {
-  StyleSheet, Text, TouchableOpacity, View
-} from 'react-native';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useSelector } from 'react-redux';
 import firebaseConfig from '../../../authBase';
 
 import ChatRoom from '../../components/chatRoom';
-import {fetchEvents} from '../../services/api';
-import { useDispatch, useSelector } from 'react-redux';
-
 
 function ChatList({ navigation }) {
-
   const auth = getAuth(firebaseConfig);
-  const {events} = useSelector((state) => state.event);
+  const { events } = useSelector((state) => state.event);
   const handleChat = (id) => {
-    navigation.navigate('Chat', { id: id});
-  }
+    navigation.navigate('Chat', { id });
+  };
 
-  const fakeData = [{id: '123', eventName: 'chats',num:"3"}, {id: '456', eventName: 'Play',num:"2"}];
+  const fakeData = [{ id: '123', eventName: 'chats', num: '3' }, { id: '456', eventName: 'Play', num: '2' }];
 
   return (
-    <View style={styles.container}>
-      <Text>
-        Email:
-        {auth.currentUser?.email}
-      </Text>
-      {events.map((item) => (
-        <TouchableOpacity onPress={()=>handleChat(item._id)} style={styles.button} key={item._id}>
-            <ChatRoom id={item._id} eventName={item.name} num={item.participants
-      }/>
-        </TouchableOpacity>
+    <ScrollView style={{ backgroundColor: '#323c47' }}>
+      <View style={styles.container}>
+        <Text>
+          Email:
+          {auth.currentUser?.email}
+        </Text>
+        {events.map((item) => (
+          <TouchableOpacity onPress={() => handleChat(item._id)} style={styles.button} key={item._id}>
+            <ChatRoom
+              id={item._id}
+              eventName={item.name}
+              num={item.participants}
+            />
+          </TouchableOpacity>
         ))}
-      {/* <TouchableOpacity
+        {/* <TouchableOpacity
         onPress={handleChat}
         style={styles.button}
       >
       <ChatRoom id={"123"} eventName={"chats"} num={"11"}/>
       </TouchableOpacity> */}
-    </View>
+      </View>
+    </ScrollView>
   );
 }
 
